67e6cf86225bbcd646cc1660fac1d21d8d09453b
Mishe12 <msheely12@devry.edu>
2015-04-16 10:11:14 -0400

    Initial finaling of NexmoClient

--- NexmoClient/Nexmo.cs
+++ NexmoClient/Nexmo.cs
@@ -11,22 +11,36 @@ namespace NexmoClient
         private string _apiKey;
         private string _apiSecret;
         private string phone;
-        private string message;
+        private string _message;
+
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }        
+
+        public string Message
+        {
+            get { return _message; }
+            set { _message = value; }
+        }
+
+        public string ApiKey
+        {
+            get { return _apiKey; }
+            set { _apiKey = value; }
+        }
+
+        public string ApiSecret
+        {
+            get { return _apiSecret; }
+            set { _apiSecret = value; }
+        }
 
         public Nexmo(string key, string secret)
         {
             this._apiKey = key;
             this._apiSecret = secret;
-        }
-
-        public void PhoneNumber(string number)
-        {
-            this.phone = number;
-        }
-
-        public void SendMessage(string message)
-        {
-            this.message = message;
         }
     }
 }
--- NexmoClient/NexmoCall.cs
+++ NexmoClient/NexmoCall.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
 
 namespace NexmoClient
 {
     class NexmoCall
     {
+        private string baseUrl = "https://api.nexmo.com/tts/json";
 
+        public void MakeCall(Nexmo nex)
+        {
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest("?api_key={key}&api_secret={secret}&to={phone}&text={message}");
+            request.AddParameter("key", nex.ApiKey);
+            request.AddParameter("secret", nex.ApiSecret);
+            request.AddParameter("phone", nex.Phone);
+            request.AddParameter("message", nex.Message.Replace(' ', '+'));
+            client.Execute(request);
+        }
     }
 }
