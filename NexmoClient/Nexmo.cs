5358a7fe2adf4210a93072b80324f2ce1708fde4
Mishe12 <msheely12@devry.edu>
2015-04-16 09:50:00 -0400

    Adding classes to facilitate calls

--- NexmoClient/Nexmo.cs
+++ NexmoClient/Nexmo.cs
@@ -8,5 +8,25 @@ namespace NexmoClient
 {
     public class Nexmo
     {
+        private string _apiKey;
+        private string _apiSecret;
+        private string phone;
+        private string message;
+
+        public Nexmo(string key, string secret)
+        {
+            this._apiKey = key;
+            this._apiSecret = secret;
+        }
+
+        public void PhoneNumber(string number)
+        {
+            this.phone = number;
+        }
+
+        public void SendMessage(string message)
+        {
+            this.message = message;
+        }
     }
 }
